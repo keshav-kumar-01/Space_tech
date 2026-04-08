8a637904cc56dbe105ea5167ab59c26da75ae9e1
keshav Kumar <keshavkumarhf@gmail.com>
2026-04-08 22:29:40 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+  plugins: [
+    wasm(),
+    topLevelAwait({
+      // The export name of top-level promise
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level promise, for example:
+      // promiseImportName: i => `__tla_${i}`
+      promiseImportName: i => `__tla_${i}`
+    })
+  ],
   server: {
     port: 3000,
     open: false,
@@ -9,6 +21,11 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
   },
   esbuild: {
     target: 'esnext',
